557bb69e6f52a821f6565440dd847ba469371800
Tom Milton <tjm@DATAHONE.co.uk>
2023-04-21 16:34:53 +0100

    initial commit

---
+++ src/main/java/com/tkhmm/application/Application.java
@@ -0,0 +1,29 @@
+package com.tkhmm.application;
+
+import com.tkhmm.application.data.service.UserRepository;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import javax.sql.DataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/tkhmm/application/data/Role.java
@@ -0,0 +1,5 @@
+package com.tkhmm.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
